def1199ee4413e7d45ed4f5dc450dd9b6f6ee7e4
Paul Massardo <paul.massardo@gmail.com>
2018-01-05 11:15:45 -0500

    Add project files.

---
+++ DemoEventsVB/DemoEventsVB.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDemoEventsVB
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblEventOutput = New System.Windows.Forms.Label()
+        Me.lbEvent = New System.Windows.Forms.Label()
+        Me.tbTextBoxDemonstrator = New System.Windows.Forms.TextBox()
+        Me.lbLabelDemonstrator = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblEventOutput
+        '
+        Me.lblEventOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblEventOutput.Location = New System.Drawing.Point(5, 74)
+        Me.lblEventOutput.Name = "lblEventOutput"
+        Me.lblEventOutput.Size = New System.Drawing.Size(268, 185)
+        Me.lblEventOutput.TabIndex = 4
+        '
+        'lbEvent
+        '
+        Me.lbEvent.Location = New System.Drawing.Point(3, 54)
+        Me.lbEvent.Name = "lbEvent"
+        Me.lbEvent.Size = New System.Drawing.Size(64, 20)
+        Me.lbEvent.TabIndex = 3
+        Me.lbEvent.Text = "Event Data:"
+        '
+        'tbTextBoxDemonstrator
+        '
+        Me.tbTextBoxDemonstrator.Location = New System.Drawing.Point(141, 2)
+        Me.tbTextBoxDemonstrator.Name = "tbTextBoxDemonstrator"
+        Me.tbTextBoxDemonstrator.Size = New System.Drawing.Size(80, 20)
+        Me.tbTextBoxDemonstrator.TabIndex = 1
+        '
+        'lbLabelDemonstrator
+        '
+        Me.lbLabelDemonstrator.Location = New System.Drawing.Point(55, 5)
+        Me.lbLabelDemonstrator.Name = "lbLabelDemonstrator"
+        Me.lbLabelDemonstrator.Size = New System.Drawing.Size(80, 20)
+        Me.lbLabelDemonstrator.TabIndex = 0
+        Me.lbLabelDemonstrator.Text = "&Demonstrator"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(198, 264)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(117, 264)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 5
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(141, 28)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(82, 23)
+        Me.btnEnter.TabIndex = 2
+        Me.btnEnter.Text = "&Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'frmDemoEventsVB
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(277, 289)
+        Me.Controls.Add(Me.lblEventOutput)
+        Me.Controls.Add(Me.lbEvent)
+        Me.Controls.Add(Me.tbTextBoxDemonstrator)
+        Me.Controls.Add(Me.lbLabelDemonstrator)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.MaximizeBox = False
+        Me.Name = "frmDemoEventsVB"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "VB Events Demonstration"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents lblEventOutput As Label
+    Private WithEvents lbEvent As Label
+    Private WithEvents tbTextBoxDemonstrator As TextBox
+    Private WithEvents lbLabelDemonstrator As Label
+    Private WithEvents btnExit As Button
+    Private WithEvents btnReset As Button
+    Private WithEvents btnEnter As Button
+End Class
---
+++ DemoEventsVB/DemoEventsVB.vb
@@ -0,0 +1,277 @@
+﻿Option Strict On
+
+Imports System.ComponentModel
+
+''' <summary>
+''' Name:           DemoEventsCS
+''' Author:         Alfred Masardo
+''' Date:           01-Jan-2018
+''' Description:    Demostrate some events and the data they produce.
+''' </summary>
+Public Class frmDemoEventsVB
+
+    ''' <summary>
+    ''' btnEnter_Click - Event that happens when the sender control Is clicked, in this case a Button.
+    '''                - Displays event data.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        ' sender Is the control that has
+        Dim control As Button = CType(sender, Button)
+
+        If (TypeOf (e) Is MouseEventArgs) Then
+
+            ' How the event was fired Like... 
+            '      * Which mouse button was pressed. 
+            '      * How many clicks. 
+            '      * X coordinate of the mouse
+            '      * Y coordinate of the mouse
+            Dim eventArguments As MouseEventArgs = CType(e, MouseEventArgs)
+
+            ' Display the event data
+            lblEventOutput.Text = "Event Handler: btnEnter_Click" & vbCrLf &
+                                    "Event: MouseDoubleClick" & vbCrLf &
+                                    "Control: " & control.Name.ToString() & vbCrLf &
+                                    "Control Data: " & control.Text & vbCrLf &
+                                    "Mouse Button Clicked: " + eventArguments.Button.ToString() & vbCrLf &
+                                    "# Of Clicks: " & eventArguments.Clicks.ToString() & vbCrLf &
+                                    "X Coordinates: " & eventArguments.X.ToString() & vbCrLf &
+                                    "Y Coordinates: " & eventArguments.Y.ToString()
+
+
+        Else ' if the user tabbed to the button and click Enter
+
+            ' since a mouse was not used there are no mouse events
+
+            ' Display the event data
+            lblEventOutput.Text = "Event Handler: btnEnter_Click" & vbCrLf &
+                                    "Event: MouseDoubleClick" & vbCrLf &
+                                    "Control: " & control.Name.ToString() & vbCrLf &
+                                    "Control Data: " & control.Text
+
+        End If
+
+    End Sub
+
+    Private Sub btnEnter_MouseHover(sender As Object, e As EventArgs) Handles btnEnter.MouseHover
+
+        ' sender Is the control that Is 
+        ' sending the event
+        ' sender Is the control that has
+        Dim control As Button = CType(sender, Button)
+
+        ' Other Event arguments do Not pass anything
+
+        ' Display the event data
+        lblEventOutput.Text = "Event Handler: btnEnter_MouseHover" & vbCrLf &
+                                    "Event: MouseHover" & vbCrLf &
+                                    "Control: " & control.Name.ToString() & vbCrLf &
+                                    "Control Data: " & control.Text
+
+    End Sub
+
+    ''' <summary>
+    ''' lbLabelDemonstrator_Click - Event that happens when the sender control Is clicked, in this case a Label.
+    '''                - Displays event data.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub lbLabelDemonstrator_Click(sender As Object, e As EventArgs) Handles lbLabelDemonstrator.Click
+
+        ' sender Is the control that has
+        Dim control As Label = CType(sender, Label)
+
+        ' How the event was fired Like... 
+        '      * Which mouse button was pressed. 
+        '      * How many clicks. 
+        '      * X coordinate of the mouse
+        '      * Y coordinate of the mouse
+        Dim eventArguments As MouseEventArgs = CType(e, MouseEventArgs)
+
+        ' Display the event data
+        lblEventOutput.Text = "Event Handler: lbLabelDemonstrator_Click" & vbCrLf &
+                                "Event: MouseDoubleClick" & vbCrLf &
+                                "Control: " & control.Name.ToString() & vbCrLf &
+                                "Control Data: " & control.Text & vbCrLf &
+                                "Mouse Button Clicked: " + eventArguments.Button.ToString() & vbCrLf &
+                                "# Of Clicks: " & eventArguments.Clicks.ToString() & vbCrLf &
+                                "X Coordinates: " & eventArguments.X.ToString() & vbCrLf &
+                                "Y Coordinates: " & eventArguments.Y.ToString()
+
+    End Sub
+    ''' <summary>
+    ''' lbLabelDemonstrator_MouseHover - Event that happens when the mouse hovers over the sender control, in this case a Label.
+    '''                                 - Displays event data.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub lbLabelDemonstrator_MouseHover(sender As Object, e As EventArgs) Handles lbLabelDemonstrator.MouseHover
+
+        ' sender Is the control that Is 
+        ' sending the event
+        ' sender Is the control that has
+        Dim control As Label = CType(sender, Label)
+
+        ' Other Event arguments do Not pass anything
+
+        ' Display the event data
+        lblEventOutput.Text = "Event Handler: lbLabelDemonstrator_MouseHover" & vbCrLf &
+                                    "Event: MouseHover" & vbCrLf &
+                                    "Control: " & control.Name.ToString() & vbCrLf &
+                                    "Control Data: " & control.Text
+
+    End Sub
+
+    ''' <summary>
+    ''' tbTextBoxDemonstrator_TextChanged - Event that happens when the Text property of the sender control, in this case a TextBox, is changed.
+    '''                                     - Displays event data.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub tbTextBoxDemonstrator_TextChanged(sender As Object, e As EventArgs) Handles tbTextBoxDemonstrator.TextChanged
+
+        ' sender Is the control that Is 
+        ' sending the event
+        ' sender Is the control that has
+        Dim control As TextBox = CType(sender, TextBox)
+
+        ' Other Event arguments do Not pass anything
+
+        ' Display the event data
+        lblEventOutput.Text = "Event Handler: tbTextBoxDemonstrator_TextChanged" & vbCrLf &
+                                    "Event: MouseHover" & vbCrLf &
+                                    "Control: " & control.Name.ToString() & vbCrLf &
+                                    "Control Data: " & control.Text
+
+    End Sub
+
+    ''' <summary>
+    ''' tbTextBoxDemonstrator_MouseHover - Event that happens when the mouse hovers over the sender control, in this case a TextBox.
+    '''                                 - Displays event data.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub tbTextBoxDemonstrator_MouseHover(sender As Object, e As EventArgs) Handles tbTextBoxDemonstrator.MouseHover
+
+        ' sender Is the control that Is 
+        ' sending the event
+        ' sender Is the control that has
+        Dim control As TextBox = CType(sender, TextBox)
+
+        ' Other Event arguments do Not pass anything
+
+        ' Display the event data
+        lblEventOutput.Text = "Event Handler: tbTextBoxDemonstrator_MouseHover" & vbCrLf &
+                                    "Event: MouseHover" & vbCrLf &
+                                    "Control: " & control.Name.ToString() & vbCrLf &
+                                    "Control Data: " & control.Text
+
+    End Sub
+
+    ''' <summary>
+    ''' btnReset_Click - Event that happens when the sender control Is clicked, in this case a Button.
+    '''                - Clears the screen.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        ' loop through the controls on the form
+        For Each control As Control In Me.Controls
+
+            If TypeOf (control) Is TextBox Then
+
+                control.Text = String.Empty
+
+            ElseIf TypeOf (control) Is Label Then
+
+                If CType(control, Label).BorderStyle = BorderStyle.Fixed3D Then
+
+                    control.Text = String.Empty
+
+                End If
+
+            End If
+
+        Next control
+
+    End Sub
+
+    ''' <summary>
+    ''' btnExit_Click - Event that happens when the sender control Is clicked, in this case a Button.
+    '''               - Commences the process of closing the form.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Exit the form
+        Me.Close()
+    End Sub
+
+    ''' <summary>
+    ''' frmDemoEventsCS_FormClosing - Event that happens just prior to the form actually closing.
+    '''                             - Checks to see if the user actually wants to close the form.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub frmDemoEventsVB_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+
+        ' Get the results from the MessageBox
+        Dim resultOne As DialogResult = MessageBox.Show("Are you sure you wish you would like to close the form?", "Form Close", MessageBoxButtons.YesNo)
+
+        ' If the user selected No
+        If resultOne = DialogResult.No Then
+
+            ' Then Cancel the Closing of the form
+            e.Cancel = True
+
+        Else
+
+            ' Get the results from the MessageBox
+            Dim resultTwo As DialogResult = MessageBox.Show("Really? You don't like me, FINE!", "Form Close", MessageBoxButtons.YesNo)
+
+            ' If the user selected No
+            If resultTwo = DialogResult.No Then
+
+                ' Then Cancel the Closing of the form
+                e.Cancel = True
+
+            End If
+
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' frmDemoEventsCS_MouseDoubleClick - Event that happens when the sender control (Form) Is double clicked.
+    '''                - Displays event data.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub frmDemoEventsVB_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Me.MouseDoubleClick
+
+        ' sender Is the control that has
+        Dim control As Form = CType(sender, Form)
+
+        ' How the event was fired Like... 
+        '      * Which mouse button was pressed. 
+        '      * How many clicks. 
+        '      * X coordinate of the mouse
+        '      * Y coordinate of the mouse
+        Dim eventArguments As MouseEventArgs = CType(e, MouseEventArgs)
+
+        ' Display the event data
+        lblEventOutput.Text = "Event Handler: frmDemoEventsVB_MouseDoubleClick" & vbCrLf &
+                                "Event: MouseDoubleClick" & vbCrLf &
+                                "Control: " & control.Name.ToString() & vbCrLf &
+                                "Control Data: " & control.Text & vbCrLf &
+                                "Mouse Button Clicked: " + eventArguments.Button.ToString() & vbCrLf &
+                                "# Of Clicks: " & eventArguments.Clicks.ToString() & vbCrLf &
+                                "X Coordinates: " & eventArguments.X.ToString() & vbCrLf &
+                                "Y Coordinates: " & eventArguments.Y.ToString()
+
+    End Sub
+
+End Class
